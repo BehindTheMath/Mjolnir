240939b56db4d501607a7aa312a4caaec0b54a80
Behind The Math <info@behindthemath.io>
2017-03-14 02:01:46 -0400

    Refactor BruteForce to store guesses as arrays of ints
    Store the current guess as an array of ints representing the position in characterSet, instead of using char[]. This minimizes the number of times we need to call arraySearch().

--- src/main/java/io/behindthemath/mjolnir/attack/BruteForce.java
+++ src/main/java/io/behindthemath/mjolnir/attack/BruteForce.java
@@ -9,7 +9,9 @@ import java.util.Arrays;
  */
 public class BruteForce implements Attack {
     private char[] characterSet;
-    private char[] currentGuess;
+    // An array of ints, each one corresponding to the index of a character in characterSet
+    private int[] currentGuessIndexes;
+    private final int characterSetMaxIndex;
 
     public BruteForce(final char[] characterSet, final int guessLength) {
         this(characterSet, guessLength, null);
@@ -17,30 +19,43 @@ public class BruteForce implements Attack {
 
     public BruteForce(char[] characterSet, int guessLength, final String lastAttempt) {
         this.characterSet = characterSet;
+        characterSetMaxIndex = characterSet.length - 1;
 
         if (lastAttempt == null) {
-            currentGuess = new char[guessLength];
-            Arrays.fill(currentGuess, this.characterSet[0]);
+            currentGuessIndexes = new int[guessLength];
+            Arrays.fill(currentGuessIndexes, 0);
         } else {
-            currentGuess = lastAttempt.toCharArray();
+            currentGuessIndexes = parseAttemptToStartFrom(lastAttempt);
         }
     }
 
+    /**
+     * Calculates the next guess
+     */
     private void increment() {
-        int index = currentGuess.length - 1;
+        // Start from the right-most character
+        int index = currentGuessIndexes.length - 1;
         while (index >= 0) {
-            if (currentGuess[index] == characterSet[characterSet.length - 1]) {
-                if (index == 0) {
-                    currentGuess = new char[currentGuess.length + 1];
-                    Arrays.fill(currentGuess, characterSet[0]);
-                    break;
-                } else {
-                    currentGuess[index] = characterSet[0];
-                    index--;
-                }
-            } else {
-                currentGuess[index] = characterSet[arraySearch(characterSet, currentGuess[index]) + 1];
+            // If we haven't yet tried every character from the character set in this character position
+            if (currentGuessIndexes[index] != characterSetMaxIndex) {
+                // Try the next character
+                currentGuessIndexes[index]++;
                 break;
+            } else {
+                // Otherwise, if we have already tried every character from the character set in this character position
+                if (index != 0) {
+                    /* If the current character position isn't the first position (left-most), that means we haven't
+                     * tried every possible permutation for this guess length. So reset current character position to
+                     * the first character from the character set, and move to the next position to the left.
+                     */
+                    currentGuessIndexes[index] = 0;
+                    index--;
+                } else {
+                    // If we've already tried every permutation for this guess length, go to the next one
+                    currentGuessIndexes = new int[currentGuessIndexes.length + 1];
+                    Arrays.fill(currentGuessIndexes, 0);
+                    break;
+                }
             }
         }
     }
@@ -48,8 +63,42 @@ public class BruteForce implements Attack {
     @Override
     public synchronized String getNextAttempt() {
         increment();
-        return String.valueOf(currentGuess);
+        return String.valueOf(buildCurrentGuess());
     }
+
+    /**
+     * Builds the next guess into a {@link String}.
+     * @return
+     */
+    private char[] buildCurrentGuess() {
+/*
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int currentGuessIndex : currentGuessIndexes) {
+            stringBuilder.append(characterSet[currentGuessIndex]);
+        }
+        return stringBuilder.toString();
+*/
+        char[] guess = new char[currentGuessIndexes.length];
+        for(int i = 0; i < guess.length; i++) {
+            guess[i] = characterSet[currentGuessIndexes[i]];
+        }
+        return guess;
+    }
+
+    /**
+     * Parses a {@link String} representing the last attempt, into an array of {@code int}s for {@code currentGuessIndexes}.
+     *
+     * @param lastAttempt A {@link String} representing the last attempt.
+     * @return An array of {@code int}s for {@code currentGuessIndex}.
+     */
+    private int[] parseAttemptToStartFrom(final String lastAttempt) {
+        int[] currentGuessIndexesBuffer = new int[lastAttempt.length()];
+        for (int i = 0; i < lastAttempt.length(); i++) {
+            currentGuessIndexesBuffer[i] = arraySearch(characterSet, lastAttempt.charAt(i));
+        }
+        return currentGuessIndexesBuffer;
+    }
+
 
     /**
      * Searches the specified array of chars for the specified value.
