2147a4c489f966ca053d1668406b9f4265c7e828
Behind The Math <info@behindthemath.io>
2017-03-16 23:39:28 -0400

    Add tests for BruteForce and AttackCallable

---
+++ src/test/java/io/behindthemath/mjolnir/attack/BruteForceTest.java
@@ -0,0 +1,60 @@
+package io.behindthemath.mjolnir.attack;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.reflect.Whitebox;
+
+import static org.junit.Assert.assertArrayEquals;
+
+/**
+ * Created by Behind The Math on 3/4/2017.
+ */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(BruteForce.class)
+public class BruteForceTest {
+    private final char[] characterSet = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w',
+            'x','y','z'};
+    private final String INCREMENT_METHOD = "increment";
+    private final String CURRENT_GUESS_INDEXES_FIELD = "currentGuessIndexes";
+
+    @Test
+    public void testIncrement() throws Exception {
+        final BruteForce bruteForce = new BruteForce(characterSet, 4);
+        Whitebox.invokeMethod(bruteForce, INCREMENT_METHOD);
+        final int[] currentGuessIndexes = Whitebox.getInternalState(bruteForce, CURRENT_GUESS_INDEXES_FIELD);
+
+        assertArrayEquals(new int[]{0, 0, 0, 1}, currentGuessIndexes);
+    }
+
+    @Test
+    public void testIncrement_whenCurrentGuessIndexesReachesCharacterSetMaxIndex_itShouldCarryOverAndResetCurrentPosition() throws Exception {
+        final BruteForce bruteForce = new BruteForce(characterSet, 4);
+        Whitebox.setInternalState(bruteForce, CURRENT_GUESS_INDEXES_FIELD, new int[]{0, 0, 0, 25});
+        Whitebox.invokeMethod(bruteForce, INCREMENT_METHOD);
+        final int[] currentGuessIndexes = Whitebox.getInternalState(bruteForce, CURRENT_GUESS_INDEXES_FIELD);
+
+        assertArrayEquals(new int[]{0, 0, 1, 0}, currentGuessIndexes);
+    }
+
+    @Test
+    public void testIncrement_whenCurrentGuessIndexesReachesCharacterSetMaxIndexForAllPositions_itShouldAddAPosition() throws Exception {
+        final BruteForce bruteForce = new BruteForce(characterSet, 4);
+        Whitebox.setInternalState(bruteForce, CURRENT_GUESS_INDEXES_FIELD, new int[]{25, 25, 25, 25});
+        Whitebox.invokeMethod(bruteForce, INCREMENT_METHOD);
+        final int[] currentGuessIndexes = Whitebox.getInternalState(bruteForce, CURRENT_GUESS_INDEXES_FIELD);
+
+        assertArrayEquals(new int[]{0, 0, 0, 0, 0}, currentGuessIndexes);
+    }
+
+    @Test
+    public void testParseAttemptToStartFrom() throws Exception {
+        String PARSE_ATTEMPT_TO_START_FROM_METHOD = "parseAttemptToStartFrom";
+
+        final BruteForce bruteForce = new BruteForce(characterSet, 4);
+        final int[] currentGuessIndexesBuffer = Whitebox.invokeMethod(bruteForce, PARSE_ATTEMPT_TO_START_FROM_METHOD, "abcd");
+
+        assertArrayEquals(new int[]{0, 1, 2, 3}, currentGuessIndexesBuffer);
+    }
+}
---
+++ src/test/java/io/behindthemath/mjolnir/run/AttackCallableTest.java
@@ -0,0 +1,60 @@
+package io.behindthemath.mjolnir.run;
+
+import io.behindthemath.mjolnir.attack.Attack;
+import io.behindthemath.mjolnir.attack.BruteForce;
+import io.behindthemath.mjolnir.source.Source;
+import io.behindthemath.mjolnir.source.keystore.KeystoreKeySource;
+import io.behindthemath.mjolnir.source.keystore.KeystoreSource;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Behind The Math on 3/16/2017.
+ */
+public class AttackCallableTest {
+    private char[] characterSet;
+    private final int guessLength = 4;
+    private String lastAttempt;
+
+    private final int threadNumber = 0;
+    private final int reportEvery = 20000;
+    private Attack attack;
+
+    private final String KEYSTORE_FILE_PATH = "Test keystore.jks";
+    private final String TEST_KEYSTORE_PASSWORD = "test";
+
+    @Test
+    public void testCall_withKeystoreSource() throws Exception {
+        characterSet = new char[] {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w',
+                'x','y','z'};
+        lastAttempt = "taaa";
+        attack = new BruteForce(characterSet, guessLength, lastAttempt);
+
+        final Source source = new KeystoreSource(KEYSTORE_FILE_PATH);
+        source.setup();
+
+        final AttackCallable attackCallable = new AttackCallable(threadNumber, reportEvery, attack, source);
+        final String result = attackCallable.call();
+
+        assertEquals(TEST_KEYSTORE_PASSWORD, result);
+    }
+
+    @Test
+    public void testCall_withKeystoreKeySource() throws Exception {
+        characterSet = new char[] {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t',
+                'u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9'};
+        lastAttempt = "tdaaa";
+        final String TEST_KEY_NAME = "Test key";
+        final String TEST_KEY_PASSWORD = "test1";
+
+        final Source source = new KeystoreKeySource(KEYSTORE_FILE_PATH, TEST_KEYSTORE_PASSWORD, TEST_KEY_NAME);
+        source.setup();
+
+        attack = new BruteForce(characterSet, guessLength, lastAttempt);
+        final AttackCallable attackCallable = new AttackCallable(threadNumber, reportEvery, attack, source);
+        final String result = attackCallable.call();
+
+        assertEquals(TEST_KEY_PASSWORD, result);
+    }
+}
